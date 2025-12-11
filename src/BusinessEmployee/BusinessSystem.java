2dbc47e3458c98ef6cc46894cf2536809a9393e0
Doha Mohamed <mdoha2236@gmail.com>
2025-12-11 14:11:52 +0200

    Update BusinessSystem.java

--- src/BusinessEmployee/BusinessSystem.java
+++ src/BusinessEmployee/BusinessSystem.java
@@ -1,7 +1,32 @@
 package BusinessEmployee;
 
-public class main {
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class BusinessSystem {
     static void main(String[] args) {
+        ArrayList<Employee>employees=new ArrayList<>();
+        employees.add(new Manager(1,"Doha",20000,1000));
+        employees.add(new SalesPerson(2,"Mohamed",25000,20.00));
+        employees.add(new Intern(3,"Ismael",15000));
+
+        ListIterator<Employee> lit=employees.listIterator();
+        int c=0;
+        while(lit.hasNext()){
+            c++;
+            System.out.println("-personal info for Employee number " +c);
+            Employee emp=lit.next();
+            emp.displayInfo();
+            if (emp instanceof CheckTheSpecificType) {
+                ((CheckTheSpecificType) emp).printRole();
+            }
+            System.out.println("The Total Salary is: " + emp.calculateSalary());
+
+        }
+
+
+
 
     }
 }
+
