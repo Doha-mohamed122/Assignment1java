3fd4b2499ae9e9261400f7eb6dd8850864cf9c28
Doha Mohamed <mdoha2236@gmail.com>
2025-12-11 14:14:51 +0200

    Update SalesPerson.java

--- src/BusinessEmployee/SalesPerson.java
+++ src/BusinessEmployee/SalesPerson.java
@@ -1,4 +1,28 @@
 package BusinessEmployee;
 
-public class SalesPerson {
+public class SalesPerson extends Employee implements  CheckTheSpecificType{
+    private double commission;
+    public SalesPerson(int id, String name, double baseSalary, double commission) {
+        super(id, name, baseSalary);
+        this.commission=commission;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary()+ commission;
+    }
+
+    @Override
+    public void printRole() {
+        System.out.println("Role: SalesPerson");
+    }
 }
+
