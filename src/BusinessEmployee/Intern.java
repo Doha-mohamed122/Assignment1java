9b03b2e88ab93fd1268d6c639350110e1dc87583
Doha Mohamed <mdoha2236@gmail.com>
2025-12-11 14:14:01 +0200

    Update Intern.java

--- src/BusinessEmployee/Intern.java
+++ src/BusinessEmployee/Intern.java
@@ -1,4 +1,18 @@
 package BusinessEmployee;
 
-public class Intern {
+public class Intern extends Employee implements  CheckTheSpecificType{
+    public Intern(int id, String name, double baseSalary) {
+        super(id, name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return 3000;
+    }
+
+    @Override
+    public void printRole() {
+        System.out.println("Role: Intern");
+    }
 }
+
